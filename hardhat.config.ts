dddad991b0e1a654c1cc76afac3471f6c96dce0b
helilabs-an <an1.hl@heli-cap.com>
2024-03-11 22:02:37 +0700

    first commit

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,5 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-const secret = require('./.secret.json');
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -18,10 +17,6 @@ const config: HardhatUserConfig = {
       allowUnlimitedContractSize: true,
       gas: 2100000,
       gasPrice: 8000000000
-    },
-    mumbai: {
-      url: `https://polygon-mumbai.g.alchemy.com/v2/${secret.projectId}`,
-      accounts: [secret.accountPrivateKey]
     }
   }
 };
